6bc99320099b5e389557eaf52a3486eda8113608
Halil <halillkesgin@gmail.com>
2023-11-15 03:06:24 +0300

    skeleton done

--- app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -2,11 +2,12 @@
 
 import { usePathname } from "next/navigation"
 import { useEffect, useState } from "react"
+import { Menu } from "lucide-react"
 
 import { useMobileSidebar } from "@/hooks/use-mobile-sidebar"
 import { Button } from "@/components/ui/button"
-import { Menu } from "lucide-react"
 import { Sheet, SheetContent } from "@/components/ui/sheet"
+
 import { Sidebar } from "./sidebar"
 
 export const MobileSidebar = () => {
--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -7,6 +7,7 @@ import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
 import { Accordion } from "@/components/ui/accordion"
+import { Skeleton } from "@/components/ui/skeleton"
 
 import { NavItem, Organization } from "./nav-item"
 
@@ -49,7 +50,15 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
     if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
         return (
             <>
-                Skeleton
+                <div className="flex items-center justify-between mb-2">
+                    <Skeleton className="h-10 w-[50%]" />
+                    <Skeleton className="h-10 w-10" />
+                </div>
+                <div className="space-y-2">
+                    <NavItem.Skeleton />
+                    <NavItem.Skeleton />
+                    <NavItem.Skeleton />
+                </div>
             </>
         )
     }
